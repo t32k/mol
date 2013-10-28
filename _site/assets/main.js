bbfaf1419077b6316c0573bd2d934dda8c654f22
t32k <ijok.ijok@gmail.com>
2013-10-28 16:16:25 +0900

    Update

--- _site/assets/main.js
+++
@@ -1,1 +0,0 @@
-$(window).scroll(function(){$(window).scrollTop()+$(window).height(),$(window).scrollTop()>100?$("body").addClass("nav-hidden"):$("body").removeClass("nav-hidden")}),$(".full img").click(function(){$(".full img").toggleClass("zoom")});
