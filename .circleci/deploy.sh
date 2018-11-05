6c9ad900f08ff9303d83b1a65cc7031d4a73e976
t32k <ijok.ijok@gmail.com>
2018-11-05 13:26:36 +0900

    Use CircleCI

---
+++ .circleci/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+DEPLOY_DIR=deploy
+
+git config --global push.default simple
+git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
+git config --global user.name $CIRCLE_USERNAME
+
+git clone -q --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
+
+cd $DEPLOY_DIR
+rsync -arv --delete ../public/* .
+
+git add -f .
+git commit -m "Deploy build $CIRCLE_BUILD_NUM [ci skip]" || true
+git push -f
